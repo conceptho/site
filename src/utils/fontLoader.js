3afdbc46d6460f94285bdb7ca712d3c256f64e00
Rômulo Guimarães <romulo1984@gmail.com>
2020-04-07 18:35:16 -0300

    Font loader over https

--- src/utils/fontLoader.js
+++ src/utils/fontLoader.js
@@ -4,7 +4,7 @@ export default () => {
   }
 
   const wf = document.createElement('script')
-  wf.src = '//ajax.googleapis.com/ajax/libs/webfont/1/webfont.js'
+  wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js'
   wf.type = 'text/javascript'
   wf.async = 'true'
   const s = document.getElementsByTagName('script')[0]
