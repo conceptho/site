c3277a9aa1d4060ed2fab37dbfb7b4672ec515d4
MayaraViana <54589016+MayaraViana@users.noreply.github.com>
2020-08-17 12:56:38 -0300

    404 mensage, tutorial, background and logo implementation

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,5 +1,16 @@
 <template>
-  <ConcepthoGame />
+  <div class="game-wrapper">
+    <div class="d-none d-lg-block">
+      <div class="tittle d-flex justify-content-center align-items-center"><h1 class="display-4">404 ERROR PAGE</h1></div>
+      <div class="game-logo d-flex justify-content-center align-items-center"><img class="img-logo" src="./../components/ConcepthoGame/Game/assets/logo.png"></div>
+      <ConcepthoGame />
+      <div class="tutorial">
+        <p>1 - Press &rarr; to select your caracter</p>
+        <p>2 - Press Enter to chosee</p>
+        <p>3 - Use &larr; &uarr; &rarr; &darr; to control</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -12,3 +23,40 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+@import "../assets/styles/general";
+.game-wrapper {
+  width: 100%;
+  height: 100vh;
+  background-image: url("./../components/ConcepthoGame/Game/assets/background.png");
+}
+
+.tittle{
+  font-family: 'Press Start 2P', cursive;
+  color: white;
+  padding-top: 2%;
+}
+.game-logo{
+  position: absolute;
+  top: 15%;
+  width: 100%;
+  height: 100px;
+  z-index: 999;
+  .img-logo{
+    height:100%;
+  }
+}
+
+.tutorial{
+  position: absolute;
+  top: 25%;
+  right: 1%;
+  width: 15%;
+  font-family: 'Press Start 2P', cursive;
+  color: white;
+  p{
+    padding-bottom: 35px;
+  }
+}
+</style>
